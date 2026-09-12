7758bb3742c284d77e6f147931d4732b49c65c20
pbwsantu-collab <pbwsantu@gmail.com>
2026-09-12 18:12:42 +0530

    Add PWA frontend: index.html, style.css, app.js, manifest, service worker

---
+++ docs/sw.js
@@ -0,0 +1,35 @@
+const CACHE_NAME = 'bb-store-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './data/lessons.json',
+  './data/questions.json',
+  './data/exercises.json',
+  './data/answers.json',
+  './data/chapters.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(keys.filter((k) => k !== CACHE_NAME).map((k) => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cached) => cached || fetch(e.request).catch(() => caches.match('./index.html')))
+  );
+});
